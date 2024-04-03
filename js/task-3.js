6cd5ee62373af4d99d3205841edfbf0b2938a147
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-04-03 21:44:05 +0300

    Update  tssk-1.js file, tssk-2.js file, tssk-3.js file

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,7 @@
 'use strict';
 function getElementWidth(content, padding, border) {
-    return(Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2);
+    return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
 }
-
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
