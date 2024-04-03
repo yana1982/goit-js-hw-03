53dfe8bc6e3abeab5ec3b2668eaf1706d8c44af6
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-04-03 11:25:12 +0300

    Update task-1.js file, task-2js file, task-3.js file

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,12 @@
 'use strict';
-function getShippingMessage(country, price, deliveryFee) {console.log(`Shipping to ${country} will cost ${price * deliveryFee} credits`);}
-getShippingMessage ("Australia", 120, 50);
-getShippingMessage ("Germany", 80, 20);
-getShippingMessage ("Sweden", 100, 20);
+function getShippingMessage(country, price, deliveryFee) {
+    return(`Shipping to ${country} will cost ${price + deliveryFee} credits`);
+}
+
+getShippingMessage("Australia", 120, 50);
+getShippingMessage("Germany", 80, 20);
+getShippingMessage("Sweden", 100, 20);
+
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
